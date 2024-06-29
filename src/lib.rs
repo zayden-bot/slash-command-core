1f385d7045b19a883a65ea38c2d26fde8fa23519
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-06-29 03:19:41 +0100

    Add initial project files and dependencies

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+use serenity::all::{Context, CreateCommand, CommandInteraction};
+
+#[async_trait]
+pub trait SlashCommand<E: std::error::Error> {
+    async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<(), E>;
+
+    fn register() -> CreateCommand;
+}
