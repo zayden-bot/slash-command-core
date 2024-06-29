66fafb64165cb3092e90b4ffbc263cdffbf2090f
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-06-29 14:20:47 +0100

    Add parse options util

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use serenity::all::{Context, CreateCommand, CommandInteraction};
+use std::collections::HashMap;
+use serenity::all::{Context, CreateCommand, CommandInteraction, ResolvedOption, ResolvedValue};
 use async_trait::async_trait;
 
 #[async_trait]
@@ -6,4 +7,16 @@ pub trait SlashCommand<E: std::error::Error> {
     async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<(), E>;
 
     fn register() -> CreateCommand;
+}
+
+pub fn parse_options<'a>(
+    options: &'a Vec<ResolvedOption<'_>>,
+) -> HashMap<&'a str, &'a ResolvedValue<'a>> {
+    let mut parsed_options = HashMap::with_capacity(options.len());
+
+    for option in options {
+        parsed_options.insert(option.name, &option.value);
+    }
+
+    parsed_options
 }
